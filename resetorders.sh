34c736d1a0dfbe346822d7b0a9ffa8f3afb01f40
Phil Tyler <phil.tyler@pantheon.io>
2023-02-23 11:16:41 -0800

    adjust run script

---
+++ resetorders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -eou pipefail
+
+
+
+terminus remote:wp pasta-la-vista.live -- post list --shop_order
--- run.sh
+++ run.sh
@@ -2,13 +2,15 @@
 set -eou pipefail
 
 # Update these numbers for our first real test, then leave consistent for all test cases
-VUS=10
-DURATION=30s
+VUS=50
+DURATION=5m
 
 # log the date before and after so we know what to look for in new relic
 date -u
 
 k6 run woo-checkout.js  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
+
+date -u
 
 # TODO: Do we want different durations or vus for this one?
 k6 run woo-customer.js  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/

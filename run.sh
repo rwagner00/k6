fb83510254b713994a8bc5fda836ed4fa6ab78b7
Ryan Wagner <ryan.wagner@pantheon.io>
2023-03-06 16:56:27 -0500

    Adjusts run time to 10m and changes tests to start on whole minutes for ease of tracking.

--- run.sh
+++ run.sh
@@ -3,12 +3,26 @@ set -eou pipefail
 
 # Update these numbers for our first real test, then leave consistent for all test cases
 VUS=50
-DURATION=5m
+DURATION=10m
+
+while [[ $(date +%S) -gt 0 ]]
+do
+    sleep 1
+done
 
 # log the date before and after so we know what to look for in new relic
 date -u
 
 k6 run woo-checkout.js  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
+echo "Test complete at: "
+date -u
+# Give the site 30 seconds to cool down and then start the test on the next full minute.
+echo "Checkout Test complete. Starting Customer test in 30s at the next whole minute."
+sleep 30
+while [[ $(date +%S) -gt 0 ]]
+do
+    sleep 1
+done
 
 date -u
 

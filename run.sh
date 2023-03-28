f3714412f53299ce3a4b7f06e341bc38cd94ffb2
Ryan Wagner <ryan.wagner@pantheon.io>
2023-03-28 10:35:16 -0400

    Updates run parameters

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 # Update these numbers for our first real test, then leave consistent for all test cases
 VUS=50
-DURATION=15m
+DURATION=5m
 
 while [[ $(date +%S) -gt 0 ]]
 do
@@ -13,7 +13,7 @@ done
 # log the date before and after so we know what to look for in new relic
 date -u
 
-k6 run woo-checkout.js  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
+k6 run woo-checkout.js --env BYPASS_CACHE=1  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
 echo "Test complete at: "
 date -u
 # Give the site 30 seconds to cool down and then start the test on the next full minute.
@@ -27,6 +27,6 @@ done
 date -u
 
 # TODO: Do we want different durations or vus for this one?
-k6 run woo-customer.js  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
+k6 run woo-customer.js --env BYPASS_CACHE=1  --vus="${VUS}" --duration="${DURATION}" --env SITE_URL=https://live-pasta-la-vista.pantheonsite.io/
 
 date -u
